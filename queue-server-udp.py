9fa8071906ee4b68ee8b3dffe9bea669cf50355e
perinze <perinzefaper@foxmail.com>
2022-04-04 10:45:58 +0800

    reset blocking

--- queue-server-udp.py
+++ queue-server-udp.py
@@ -1,7 +1,7 @@
 import socket
 import queue
 import re
-from tokenize import group
+import select
 
 host = 'localhost'
 port = 11451
@@ -11,10 +11,14 @@ class QueueServer(object):
 	def __init__(self, host, port, tmp_size=1024):
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.sock.bind((host, port))
-		self.sock.setblocking(False)
+		# self.sock.setblocking(False)
+		# self.epoll = select.epoll()
+		# self.epoll.register(self.sock.fileno(), select.EPOLLIN)
+
 		self.buf = ''
 		self.TMP_SIZE = tmp_size
 		self.queue = queue.Queue()
+
 		self.regex = r"\^([\d,]+)\$"
 		self.delim = r","
 
@@ -24,18 +28,20 @@ class QueueServer(object):
 		return self.queue.get()
 
 	def parse_buf(self):
-		while True:
-			tmp, _ = self.sock.recvfrom(self.TMP_SIZE)
-			if not tmp:
-				break
-			self.buf = ''.join([self.buf, bytes.decode(tmp)])
+		self.recv()
 		while True:
 			match = re.search(self.regex, self.buf)
+			if not match:
+				break
 			_, end = match.span()
 			self.buf = self.buf[end:]
 			substr = match.group(1)
 			data = list(map(int, re.split(self.delim, substr)))
 			self.queue.put(data)
+
+	def recv(self):
+		tmp, _ = self.sock.recvfrom(self.TMP_SIZE)
+		self.buf = ''.join([self.buf, bytes.decode(tmp)])
 
 if __name__ == '__main__':
 	qs = QueueServer(host, port)
